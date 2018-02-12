ce86def713a8bd9b59fdec9ad5cc3dba00b5877d
yangyang2018 <shayu2017@163.com>
2018-02-12 16:17:16 +0800

    first commit

---
+++ biz-datasource/src/main/java/com/wps/pp/share/base/App.java
@@ -0,0 +1,13 @@
+package com.wps.pp.share.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
