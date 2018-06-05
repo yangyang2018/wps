0bb42d718fcf5514d90c9db9f3b82833df3b2f3f
yangyang2108 <shayu2017@163.com>
2018-06-05 17:45:13 +0800

    quartz

---
+++ common-quartz/src/main/java/com/wps/pp/undertake/App.java
@@ -0,0 +1,13 @@
+package com.wps.pp.undertake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
